d9f8f2fac01a38609bf537ccc096bce3f63f1a04
Pedro Henrique <pedromonteiromm@gmail.com>
2024-10-24 08:59:11 -0300

    Atualizacoes arrow icon

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const containerCarrocelHeader = document.querySelector(".container-carrocel-head
 const menuDropdown = document.querySelector(".menu-dropdown");
 const containerHeader = document.querySelector(".container-header")
 const dropdownDiv = document.querySelector(".dropdown-div")
-const dropdownDivItens = document.querySelector(".dropdown-li-itens")
-
+const dropdownDivItens = document.querySelectorAll(".dropdown-li-itens")
+const dropdownLi = document.querySelectorAll(".dropdown-li")
 
 containerMenuhamburguer.addEventListener("click", (e) => {
     containerCarrocelHeader.classList.toggle('hidden')
@@ -12,6 +12,19 @@ containerMenuhamburguer.addEventListener("click", (e) => {
     containerHeader.classList.toggle('no-overflow')
 })
 
-dropdownDiv.addEventListener("click", (e) => {
-    dropdownDivItens.classList.toggle('hidden')
-})
+
+dropdownLi.forEach(liItens => {
+    liItens.addEventListener('click', (e) => {
+        const relatedDropdownItems = liItens.nextElementSibling
+        const arrowIcon = liItens.querySelector("img")
+
+        if (relatedDropdownItems && relatedDropdownItems.classList.contains('dropdown-itens')) {
+            relatedDropdownItems.classList.toggle('hidden');
+        }
+
+        // Alterna a rotação da seta
+        if (arrowIcon) {
+            arrowIcon.classList.toggle('rotate');
+        }
+    })
+});
